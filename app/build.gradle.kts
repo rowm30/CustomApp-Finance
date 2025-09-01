5092c033a268f13d6474f10fcf7d2b8a599d6722
Kumar Mayank Verma <44160186+rowm30@users.noreply.github.com>
2025-09-01 13:05:44 +0530

    chore: target latest SDK for SMS tracker

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 
 android {
     namespace = "com.mayank.myapplication"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.mayank.myapplication"
@@ -37,11 +37,11 @@ android {
         kotlinCompilerExtensionVersion = "1.5.13"
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "11"
+        jvmTarget = "17"
     }
 }
 
